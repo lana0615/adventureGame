af0e6a8edb3604a165755c453ae1ae08db9d037f
Alana Fortin <Alanfort577@ed.amdsb.ca>
2022-11-04 11:26:26 -0400

    Add project files.

---
+++ adventureGame/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace adventureGame
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.optionLabel1 = new System.Windows.Forms.Label();
+            this.optionLabel2 = new System.Windows.Forms.Label();
+            this.optionLabel3 = new System.Windows.Forms.Label();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.option3Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.Black;
+            this.outputLabel.Location = new System.Drawing.Point(11, 20);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(558, 122);
+            this.outputLabel.TabIndex = 0;
+            // 
+            // optionLabel1
+            // 
+            this.optionLabel1.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionLabel1.Location = new System.Drawing.Point(214, 315);
+            this.optionLabel1.Name = "optionLabel1";
+            this.optionLabel1.Size = new System.Drawing.Size(355, 21);
+            this.optionLabel1.TabIndex = 1;
+            this.optionLabel1.Text = "outputLabel";
+            // 
+            // optionLabel2
+            // 
+            this.optionLabel2.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionLabel2.Location = new System.Drawing.Point(214, 365);
+            this.optionLabel2.Name = "optionLabel2";
+            this.optionLabel2.Size = new System.Drawing.Size(341, 21);
+            this.optionLabel2.TabIndex = 2;
+            // 
+            // optionLabel3
+            // 
+            this.optionLabel3.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionLabel3.Location = new System.Drawing.Point(214, 411);
+            this.optionLabel3.Name = "optionLabel3";
+            this.optionLabel3.Size = new System.Drawing.Size(268, 21);
+            this.optionLabel3.TabIndex = 3;
+            // 
+            // option1Button
+            // 
+            this.option1Button.BackColor = System.Drawing.Color.DarkCyan;
+            this.option1Button.Location = new System.Drawing.Point(104, 311);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(91, 31);
+            this.option1Button.TabIndex = 4;
+            this.option1Button.UseVisualStyleBackColor = false;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // option2Button
+            // 
+            this.option2Button.BackColor = System.Drawing.Color.DarkCyan;
+            this.option2Button.Location = new System.Drawing.Point(104, 361);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(91, 31);
+            this.option2Button.TabIndex = 5;
+            this.option2Button.UseVisualStyleBackColor = false;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // option3Button
+            // 
+            this.option3Button.BackColor = System.Drawing.Color.DarkCyan;
+            this.option3Button.Location = new System.Drawing.Point(104, 407);
+            this.option3Button.Name = "option3Button";
+            this.option3Button.Size = new System.Drawing.Size(91, 31);
+            this.option3Button.TabIndex = 6;
+            this.option3Button.UseVisualStyleBackColor = false;
+            this.option3Button.Click += new System.EventHandler(this.option3Button_Click);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(581, 475);
+            this.Controls.Add(this.option3Button);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Controls.Add(this.optionLabel3);
+            this.Controls.Add(this.optionLabel2);
+            this.Controls.Add(this.optionLabel1);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "form1";
+            this.ShowIcon = false;
+            this.Text = "dating game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label optionLabel1;
+        private System.Windows.Forms.Label optionLabel2;
+        private System.Windows.Forms.Label optionLabel3;
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Button option3Button;
+    }
+}
+
---
+++ adventureGame/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+/// Alana Fortin
+/// October 2022
+/// adventure game
+
+namespace adventureGame
+{
+    public partial class form1 : Form
+    {
+        // Tracks what page of the story the user is at
+        int page = 1;
+        public form1()
+        {
+            InitializeComponent();
+            //Display initial message and options
+            outputLabel.Text = "who would you like to go on a date with?";
+            optionLabel1.Text = "Flynn Ryder";
+            optionLabel2.Text = "Merida";
+            optionLabel3.Text = "Lightning McQueen";
+
+        }
+
+
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+            /// Check what page we are currently on, and then flip
+            /// to the page you need to go to if you selected option 1
+
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 3;
+            }
+            else if (page == 3)
+            {
+                page = 4;
+            }
+            else if (page == 4)
+            {
+                page = 5;
+            }
+            else if (page == 5)
+            {
+                page = 6;
+            }
+            else if (page == 6)
+            {
+                Random randGen = new Random();
+                int chance = randGen.Next(1, 101);
+                if (chance < 50)
+                {
+                    page = 7;
+                }
+                else
+                {
+                    page = 8;
+                }
+            }
+            else if (page == 7)
+            {
+                page = 1;
+                option3Button.Enabled = true;
+                optionLabel3.Visible = true;
+                option3Button.Visible = true;
+            }
+            else if (page == 8)
+            {
+                page = 1;
+                option3Button.Enabled = true;
+                optionLabel3.Visible = true;
+                option3Button.Visible = true;
+            }
+            else if (page == 13)
+            {
+                page = 19;
+            }
+            else if (page == 20)
+            {
+                page = 1;
+            }
+
+            /// Display text and game options to screen based on the 
+            /// current page
+            switch (page)
+            {
+                case 1:
+                    optionLabel1.Text = "Flynn Ryder";
+                    outputLabel.Text = "Who would you like to go on a date with?";
+                    optionLabel1.Text = "Flynn";
+                    optionLabel2.Text = "Merida";
+                    optionLabel3.Text = "Lightning McQueen";
+                    break;
+                case 2:
+                    outputLabel.Text = "What would you like to do on the date?";
+                    optionLabel1.Text = "Boat ride to see the lanterns";
+                    optionLabel2.Text = "Attend an art class";
+                    option3Button.Enabled = false;
+                    optionLabel3.Visible = false;
+                    break;
+                case 3:
+                    outputLabel.Text = "You are having an amazing time! He tries to grab your hand. Do you accept?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    optionLabel3.Visible = false;
+                    option3Button.Visible = false;
+                    break;
+                case 4:
+                    outputLabel.Text = "You guys are now holding hands! He asks if you want to go on a second date. What do you say?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    optionLabel3.Visible = false;
+                    option3Button.Visible = false;
+                    break;
+                case 5:
+                    outputLabel.Text = "The date ends and he texts you the next morning asking if you want to hangout with him that night. Do you respond?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    optionLabel3.Visible = false;
+                    option3Button.Visible = false;
+                    break;
+                case 6:
+                    outputLabel.Text = "You hangout with him that night and he asks you to be his gf. Do you say yes?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    optionLabel3.Visible = false;
+                    option3Button.Visible = false;
+                    break;
+                case 7:
+                    outputLabel.Text = "You two date for years!! on your three month anniversary. ";
+                    optionLabel1.Text = "";
+                    optionLabel2.Text = "";
+                    optionLabel3.Visible = false;
+                    option3Button.Visible = false;
+                    Thread.Sleep(2000);
+                    Refresh();
+                    outputLabel.Text += "He proposed and you are now engaged!! you live happy ever after! play again?";
+                    optionLabel1.Text += "Yes";
+                    optionLabel2.Text += "No";
+                    break;
+                case 8:
+                    outputLabel.Text = "You two date for years!! on your three month anniversary.";
+                    optionLabel1.Text = "";
+                    optionLabel2.Text = "";
+                    optionLabel3.Visible = false;
+                    option3Button.Visible = false;
+                    Thread.Sleep(2000);
+                    Refresh();
+                    outputLabel.Text += "he breaks up with you. Play again?";
+                    optionLabel1.Text += "Yes";
+                    optionLabel2.Text += "No";
+                    break;
+                case 19:
+                    outputLabel.Text = "You guys kiss! Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+            }
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            // seccond options for date with flynn
+            if (page == 3)
+            {
+                page = 9;
+            }
+            else if (page == 4)
+            {
+                page = 10;
+            }
+            else if (page == 5)
+            {
+                page = 11;
+            }
+            else if (page == 6)
+            {
+                page = 12;
+            }
+            else if (page == 2)
+            {
+                page = 13;
+            }
+            else if (page == 13)
+            {
+                page = 18;
+            }
+            else if (page == 1 )
+            {
+                page = 20;
+            }
+            else if (page == 20)
+            {
+                page = 1;
+            }
+            else if (page == 20)
+            {
+                page = 21;
+            }
+            else if (page == 21)
+            {
+                page = 1;
+            }
+
+            switch (page)
+            {
+                case 3:
+                    outputLabel.Text = "You are having an amazing time! He tries to grab your hand. Do you accept?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 10:
+                    outputLabel.Text = "He Pushes you over board and you drown. Play Again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 11:
+                    outputLabel.Text = "You leave the date and ghost him. Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 13:
+                    outputLabel.Text = "";
+                    optionLabel1.Text = "";
+                    optionLabel2.Text = "";
+                    break;
+                case 20:
+                    outputLabel.Text = "She never showed up. go cry at home. Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    option3Button.Visible = false;
+                    optionLabel3.Visible = false;
+                    break;
+                case 21:
+                    outputLabel.Text = " Thank you for playing";
+                    optionLabel1.Text = "";
+                    optionLabel2.Text = "";
+                    option3Button.Visible = true;
+                    optionLabel3.Visible = true;
+                    break;
+                
+                    
+            }
+        }
+
+        private void option3Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 30;
+            }
+
+            switch (page)
+            {
+                case 30:
+                    outputLabel.Text = "He never showed up. Go cry at home Play Again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    option3Button.Visible = false;
+                    optionLabel3.Visible = false;
+                    break;
+
+            }
+        }
+
+
+    }
+}
+
